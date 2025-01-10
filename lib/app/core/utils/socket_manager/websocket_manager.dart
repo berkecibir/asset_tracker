2a102c16da496ccb17b4813e972a4dae473cb51b
Berke Atahan <berkecibir@gmail.com>
2025-01-10 15:53:48 +0300

    Refactor WebSocketManager for improved structure

--- lib/app/core/utils/socket_manager/websocket_manager.dart
+++ lib/app/core/utils/socket_manager/websocket_manager.dart
@@ -1,33 +1,58 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebsocketManager {
-  final String url =
-      "wss://socket.haremaltin.com/socket.io/?EIO=4&transport=websocket";
+  late final String _url;
   late WebSocketChannel _channel;
+  webSocketManager() {
+    _url = dotenv.env['SOCKET_URL'] ?? '';
+    if (_url.isEmpty) {
+      throw Exception("WebSocket URL is not defined in .env file.");
+    }
+  }
 
   void connect() {
-    _channel = WebSocketChannel.connect(Uri.parse(url));
-    _channel.sink.add('40');
+    try {
+      _channel = WebSocketChannel.connect(Uri.parse(_url));
+      _channel.sink.add('40');
+    } catch (e) {
+      debugPrint('Error connecting to WebSocket: $e');
+    }
   }
 
   Stream<dynamic> listen() {
-    return _channel.stream.map(
-      (event) {
+    return _channel.stream.map((event) {
+      try {
         if (event is String) {
-          return jsonDecode(event);
+          if (event.startsWith('42[')) {
+            return jsonDecode(event.substring(2));
+          }
+          return event;
         }
-        return null;
-      },
-    );
+      } catch (e) {
+        debugPrint("Error processing WebSocket event: $e");
+      }
+      return null;
+    });
   }
 
   void send(String message) {
-    _channel.sink.add(message);
+    try {
+      _channel.sink.add(message);
+    } catch (e) {
+      debugPrint("Error sending message: $e");
+    }
   }
 
   void disconnect() {
-    _channel.sink.close();
+    try {
+      _channel.sink.close();
+      debugPrint("WebSocket disconnected.");
+    } catch (e) {
+      debugPrint("Error disconnecting WebSocket: $e");
+    }
   }
 }
